f59667b27b5a0c18246c7a90b1fee891cdfec73a
Diog007 <diogo.mz123@gmail.com>
2024-03-25 10:58:51 -0300

    Method of creating image URL

--- src/main/java/io/github/imageapi/application/images/ImagesController.java
+++ src/main/java/io/github/imageapi/application/images/ImagesController.java
@@ -1,19 +1,19 @@
 package io.github.imageapi.application.images;
 
 import io.github.imageapi.domain.entity.Image;
-import io.github.imageapi.domain.enums.ImageExtension;
 import io.github.imageapi.domain.service.ImageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -23,26 +23,24 @@ import java.util.List;
 public class ImagesController {
 
     private final ImageService service;
+    private final ImagesMapper mapper;
 
     @PostMapping
-    public ResponseEntity saveImage(@RequestParam("file") MultipartFile file,
-                                    @RequestParam("name") String name,
-                                    @RequestParam("tags") List<String> tags
+    public ResponseEntity saveImage(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("name") String name,
+            @RequestParam("tags") List<String> tags
             ) throws IOException {
-        log.info("Imagem recebida: name: {}, size: {}", file.getOriginalFilename(), file.getSize());
 
-        MediaType.valueOf(file.getContentType());
+        Image image = mapper.mapToImage(file, name, tags);
+        Image savedImage = service.save(image);
+        URI imageURI = buildImageURL(savedImage);
 
-        Image image = Image.builder()
-                .name(name)
-                .tag(String.join(",", tags))
-                .size(file.getSize())
-                .extension(ImageExtension.valueOf(MediaType.valueOf(file.getContentType())))
-                .file(file.getBytes())
-                .build();
+        return ResponseEntity.created(imageURI).build();
+    }
 
-        service.save(image);
-
-        return ResponseEntity.ok().build();
+    private URI buildImageURL(Image image){
+        String imagePath = "/" + image.getId();
+        return ServletUriComponentsBuilder.fromCurrentRequest().path(imagePath).build().toUri();
     }
 }
---
+++ src/main/java/io/github/imageapi/application/images/ImagesMapper.java
@@ -0,0 +1,25 @@
+package io.github.imageapi.application.images;
+
+import io.github.imageapi.domain.entity.Image;
+import io.github.imageapi.domain.enums.ImageExtension;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class ImagesMapper {
+
+    public Image mapToImage(MultipartFile file, String name, List<String> tags) throws IOException {
+
+        return Image.builder()
+                .name(name)
+                .tag(String.join(",", tags))
+                .size(file.getSize())
+                .extension(ImageExtension.valueOf(MediaType.valueOf(file.getContentType())))
+                .file(file.getBytes())
+                .build();
+    }
+}
