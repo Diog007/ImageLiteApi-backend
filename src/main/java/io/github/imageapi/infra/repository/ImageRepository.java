7846131e135b39b429314414b6f7cf4571aa7cae
Diog007 <diogo.mz123@gmail.com>
2024-03-26 14:58:10 -0300

    Created search logic in the repository

--- src/main/java/io/github/imageapi/infra/repository/ImageRepository.java
+++ src/main/java/io/github/imageapi/infra/repository/ImageRepository.java
@@ -1,7 +1,34 @@
 package io.github.imageapi.infra.repository;
 
 import io.github.imageapi.domain.entity.Image;
+import io.github.imageapi.domain.enums.ImageExtension;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.util.StringUtils;
 
-public interface ImageRepository extends JpaRepository<Image, String> {
+import java.util.List;
+
+public interface ImageRepository extends JpaRepository<Image, String>, JpaSpecificationExecutor<Image> {
+
+    default List<Image> findByExtensionAndNameOrTagsLike(ImageExtension extension, String query){
+        Specification<Image> conjunction = (root, q, criteriaBiulder) -> criteriaBiulder.conjunction();
+        Specification<Image> spec = Specification.where( conjunction );
+
+        if (extension != null){
+            Specification<Image> extensionEqual = (root, q, cb) -> cb.equal(root.get("extension"), extension);
+            spec = spec.and(extensionEqual);
+        }
+
+        if (StringUtils.hasText(query)){
+            Specification<Image> nameLike = (root, q, cb) -> cb.like(cb.upper(root.get("name")), "%" + query.toUpperCase() + "%");
+            Specification<Image> tagsLike = (root, q, cb) -> cb.like(cb.upper(root.get("name")), "%" + query.toUpperCase() + "%");
+
+            Specification<Image> nameOrTagsLike = Specification.anyOf(nameLike, tagsLike);
+
+            spec = spec.and(nameOrTagsLike);
+        }
+
+        return findAll(spec);
+    }
 }
