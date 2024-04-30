1981535fb785f973e944add59adacc45e23e6f32
Diog007 <diogo.mz123@gmail.com>
2024-04-30 02:27:32 -0300

    Created custom execution handling

---
+++ src/main/java/io/github/imageapi/domain/exception/DuplicatedTupleException.java
@@ -0,0 +1,7 @@
+package io.github.imageapi.domain.exception;
+
+public class DuplicatedTupleException extends RuntimeException{
+    public DuplicatedTupleException(String message) {
+        super(message);
+    }
+}
