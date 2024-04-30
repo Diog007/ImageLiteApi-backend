1957b7fea778b10d590688756fd61f917a2a53e7
Diog007 <diogo.mz123@gmail.com>
2024-04-30 02:26:26 -0300

    Created AccesToken class

---
+++ src/main/java/io/github/imageapi/domain/AccessToken.java
@@ -0,0 +1,10 @@
+package io.github.imageapi.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AccessToken {
+    private String accessToken;
+}
