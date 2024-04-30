922f34000437cb5c7288670c1b34ae99882c24ed
Diog007 <diogo.mz123@gmail.com>
2024-04-30 02:08:36 -0300

    Created User repository

---
+++ src/main/java/io/github/imageapi/infra/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package io.github.imageapi.infra.repository;
+
+import io.github.imageapi.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+}
